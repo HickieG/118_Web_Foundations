d6ec3d338e22169c669799ef6aabce1a92a4102c
HickieG <garrettjhickie@gmail.com>
2026-09-10 12:09:58 -0500

    adding function and improving conditional

--- Week_4/firstjavacode.js
+++ Week_4/firstjavacode.js
@@ -5,14 +5,22 @@ console.log("Hello, world! Let's cook some breakfast!");
 const eggs = true;
 const bacon = true;
 const toast = true;
+const OJ = true;
 
 console.log("Eggs status:", eggs);
 console.log("Bacon status:", bacon);
 console.log("Toast status:", toast);
+console.log("OJ status:", OJ);
 
-if (eggs && bacon && toast) {
-    console.log("Ready for a delicious breakfast!");
+function breakfastOrder(mainCourse, sideDish, bread, drink) {
+    if (mainCourse && sideDish && bread && drink) {
+        console.log("Ready for a delicious breakfast!");
+    }
+    else {
+        console.log("Something is missing for breakfast!");
+    }
 }
-else {
-    console.log("Something is missing for breakfast!");
-}
+
+//Changed the conditional statement to a function that is called below. 
+
+breakfastOrder(eggs, bacon, toast, OJ);
