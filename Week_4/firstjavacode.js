4f82da11a22270c13618f2bb026a6f49329e9100
HickieG <garrettjhickie@gmail.com>
2026-09-09 19:20:52 -0500

    adding all folders to overarching repo TAKE 1

---
+++ Week_4/firstjavacode.js
@@ -0,0 +1,18 @@
+// This is my first JavaScript code file; it feels like we are introducing Python to web development, I am here for it!
+
+console.log("Hello, world! Let's cook some breakfast!");
+
+const eggs = true;
+const bacon = true;
+const toast = true;
+
+console.log("Eggs status:", eggs);
+console.log("Bacon status:", bacon);
+console.log("Toast status:", toast);
+
+if (eggs && bacon && toast) {
+    console.log("Ready for a delicious breakfast!");
+}
+else {
+    console.log("Something is missing for breakfast!");
+}
